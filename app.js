c1ca4da1a17c8364f95b8419fe9d4baf55780ea6
Howard <dangelohoward@gmail.com>
2023-03-13 11:40:13 -0400

    Second version

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ form.addClass('centerAlign');
 
 form.on('submit', function(e){
     e.preventDefault();
-    list.append(`<li class="movies">${$('#MovieName').val()} ${$('#MovieRating').val()} stars</li>`);
-    
+    const listItem = $(`<li class="movies">${$('#MovieName').val()} ${$('#MovieRating').val()} stars</li>`);
+    const removeButton = $('<button class="remove">X</button>');
+    listItem.append(removeButton);
+    list.append(listItem);
+
     
 })
-$('li').append('<button>X</button>');
+
 
 list.on('click', function(e){
+    if(e.target.tagName === 'BUTTON'){
+        e.target.parentNode.remove();
+    }
     
-    e.target.remove();
 })
